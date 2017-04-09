77b78f4e12cf595792bcb7427db6c6e0f4af9d45
Fuxing <Lohfuxing@gmail.com>
2017-04-09 18:55:45 +0800

    Update MXSegmentedPager.h

--- MXSegmentedPager/MXSegmentedPager.h
+++ MXSegmentedPager/MXSegmentedPager.h
@@ -22,6 +22,7 @@
 
 #import <UIKit/UIKit.h>
 #import <HMSegmentedControl/HMSegmentedControl.h>
+#import <MXParallaxHeader/MXScrollView.h>
 #import <MXPagerView/MXPagerView.h>
 #import <MXParallaxHeader/MXParallaxHeader.h>
 
